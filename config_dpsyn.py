4fc9bb5a05e310b4d8aec461911931e4566268d7
Danyu Sun <sdy@Danyus-MacBook-Pro.local>
2025-09-19 12:09:14 -0700

    update

--- config_dpsyn.py
+++ config_dpsyn.py
@@ -1,14 +1,14 @@
 # path related constant
-PROJECT_PATH = "/home/dsun/NetDPSyn/"
-RAW_DATA_PATH = "/home/dsun/NetDPSyn/temp_data/raw_data/"
-PROCESSED_DATA_PATH = "/home/dsun/NetDPSyn/temp_data/processed_data/"
-SYNTHESIZED_RECORDS_PATH = "/home/dsun/NetDPSyn/temp_data/synthesized_records/"
-MARGINAL_PATH = "/home/dsun/NetDPSyn/temp_data/marginal/"
-DEPENDENCY_PATH = '/home/dsun/NetDPSyn/temp_data/dependency/'
+# PROJECT_PATH = "NetDPSyn/"
+RAW_DATA_PATH = "temp_data/raw_data/"
+PROCESSED_DATA_PATH = "temp_data/processed_data/"
+SYNTHESIZED_RECORDS_PATH = "temp_data/synthesized_records/"
+MARGINAL_PATH = "temp_data/marginal/"
+DEPENDENCY_PATH = 'temp_data/dependency/'
 
 ALL_PATH = [RAW_DATA_PATH, PROCESSED_DATA_PATH, SYNTHESIZED_RECORDS_PATH, MARGINAL_PATH, DEPENDENCY_PATH]
 
 
 # config file path
-TYPE_CONIFG_PATH = "/home/dsun/NetDPSyn/fields.json"
+TYPE_CONIFG_PATH = "fields.json"
 MARGINAL_INIT = [('srcport', 'proto', 'flag'), ('dstport', 'proto', 'flag')]
